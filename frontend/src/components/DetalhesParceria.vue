c8c1528dc107819993a1c2cab6b19c7301e0a958
jasondavindev <jasondavin.dev@gmail.com>
2019-06-02 14:37:37 -0300

    adicionado layout tela parceria

--- frontend/src/components/DetalhesParceria.vue
+++ frontend/src/components/DetalhesParceria.vue
@@ -1,12 +1,23 @@
 <template>
-  <div id="detalhe-parcerias">
-    <div id="passo-1">
-      <b>A primeira mensalidade equivale a pré-matrícula do Quero Bolsa.</b>
+  <div id="detalhe-parcerias" class="container">
+    <div id="header">
+      <div id="breadcrumb">
+        <p>/detalhes da parceria</p>
+      </div>
+    </div>
+    <div class="detalhe-parcerias-conteudo" id="passo-1">
+      <h2>A primeira mensalidade equivale a pré-matrícula do Quero Bolsa.</h2>
       <p>Das 12 parcelas, a primeira é do Quero Bolsa e as outras 11 são da sua escola. Também recomendamos que a matrícula seja isenta</p>
 
       <div class="buttons">
-        <button @click="discordar">Discordo</button>
-        <button @click="concordar">Concordo</button>
+        <div class="row">
+          <div class="col s6">
+            <button @click="discordar" class="button-cta button-grid-back">Discordo</button>
+          </div>
+          <div class="col s6">
+            <button @click="concordar" class="button-cta">Concordo</button>
+          </div>
+        </div>
       </div>
 
       <div id="mensagem-confirmacao" v-if="discorda">
@@ -14,7 +25,7 @@
           <p>Estou ciente que cobrando mais de 11 parcelas gero uma barreira de compra para o aluno. O mesmo terá que pagar no mesmo mês mensalidade e pré-matrícula.</p>
 
           <div class="buttons">
-            <button @click="alterarIsencoes">Alterar mesmo assim</button>
+            <button @click="alterarIsencoes" class="button-cta button-cta-underline">Alterar mesmo assim</button>
           </div>
         </div>
 
@@ -22,17 +33,22 @@
           <h2>O que você deseja alterar?</h2>
           <div class="campos">
             <p>
-              <input id="isencao-matricula" type="checkbox" v-model="isentarMatricula">
-              <label for="isencao-matricula">Quero isentar a matrícula</label>
+              <label>
+                <input id="isencao-matricula" type="checkbox" v-model="isentarMatricula" class="filled-in" checked="checked">
+                <span>Quero isentar a matrícula</span>
+              </label>
             </p>
             <p>
-              <input id="isencao-mensalidade" type="checkbox" v-model="isentarMensalidade">
-              <label for="isencao-mensalidade">Quero isentar a primeira mensalidade</label>
+              <label>
+
+                <input id="isencao-mensalidade" type="checkbox" v-model="isentarMensalidade" class="filled-in" checked="checked">
+                <span>Quero isentar a primeira mensalidade</span>
+              </label>
             </p>
           </div>
 
           <div class="buttons">
-            <button @click="enviar">Finalizar e enviar</button>
+            <button @click="enviar" class="button-cta">Finalizar e enviar</button>
           </div>
         </div>
       </div>
