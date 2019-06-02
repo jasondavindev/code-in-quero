f50614628a39b28eab5f6a5114c5d988c01fba40
jasondavindev <jasondavin.dev@gmail.com>
2019-06-02 02:42:10 -0300

    Tela Inicio OK

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,6 +13,7 @@ import BootstrapVue from 'bootstrap-vue';
 import './assets/styles/style.css';
 import 'materialize-css/dist/css/materialize.css';
 import Vue from 'vue';
+
 Vue.use(BootstrapVue);
 
 export default {
@@ -18,3 +21,16 @@ export default {
   components: {}
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.4s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+</style>
---
+++ frontend/src/components/Inicio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="inicio" class="container">
+    <div id="inicio-icon">
+      <img src="../assets/images/icon_inicio.png" title="Selfdesk" />
+    </div>
+    <h1>Bem-vindo ao Selfdesk</h1>
+
+    <p>Aqui você cadastra e precifica seus cursos de forma simples, fácil e rápida.</p>
+
+    <router-link :to="{name: 'escola'}" tag="button" class="button-cta">Vamos começar!</router-link>
+
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="scss">
+</style>
